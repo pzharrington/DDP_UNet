4e2aaff76b443d5943662b5ed0fae635ac2db66b
Peter Harrington <pharrington@lbl.gov>
2020-03-24 13:14:53 -0700

    Added pytorch rotation logic

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -86,3 +86,102 @@ class RandomRotator(object):
         return self.rot[rand](x)
 
 
+
+
+class RandomCropDataset_PTflip(Dataset):
+    """Random crops, using pytorch logic for rotations"""
+    def __init__(self, params, Nbody, Hydro):
+        self.Hydro = Hydro
+        self.Nbody = Nbody
+        self.length = Nbody.shape[1]
+        self.size = params.data_size
+        self.Nsamples = 1000
+        self.rotate = RandomRotator_PTflip()
+
+    def __len__(self):
+        return self.Nsamples
+
+    def __getitem__(self, idx):
+        x = np.random.randint(low=0, high=self.length-self.size)
+        if self.length-self.size-self.size==0:
+            y = 0
+        else:
+            y = np.random.randint(low=0, high=self.length-self.size-self.size)
+        z = np.random.randint(low=0, high=self.length-self.size)
+        inp = self.Nbody[:, x:x+self.size, y:y+self.size, z:z+self.size]
+        tar = self.Hydro[:, x:x+self.size, y:y+self.size, z:z+self.size]
+        rand = np.random.randint(low=1, high=25)
+        return self.rotate(torch.as_tensor(inp), rand), self.rotate(torch.as_tensor(tar), rand)
+
+
+def flip_along(x, dims):
+    for dim in dims:
+        x = flip_(x, dim)
+    return x
+
+def flip_(x, dim):
+    indices = [slice(None)] * x.dim()
+    indices[dim] = torch.arange(x.size(dim) - 1, -1, -1)
+    return x[tuple(indices)]
+
+class RandomRotator_PTflip(object):
+    """Composable transform that applies random 3D rotations by right angles in PT.
+       Adapted from tf code:
+       https://github.com/doogesh/halo_painting/blob/master/wasserstein_halo_mapping_network.ipynb"""
+
+    def __init__(self):
+        # Using pytorch flip() fn
+        self.rot = {1:  lambda x: x.flip([1,2]),
+                    2:  lambda x: x.flip([1,3]),
+                    3:  lambda x: x.flip([2,3]),
+                    4:  lambda x: x.permute([0, 2, 1, 3]).flip([1]),
+                    5:  lambda x: x.permute([0, 2, 1, 3]).flip([1,3]),
+                    6:  lambda x: x.permute([0, 2, 1, 3]).flip([2]),
+                    7:  lambda x: x.permute([0, 2, 1, 3]).flip([2,3]),
+                    8:  lambda x: x.permute([0, 3, 2, 1]).flip([1]),
+                    9:  lambda x: x.permute([0, 3, 2, 1]).flip([1,2]),
+                    10: lambda x: x.permute([0, 3, 2, 1]).flip([3]),
+                    11: lambda x: x.permute([0, 3, 2, 1]).flip([2,3]),
+                    12: lambda x: x.permute([0, 1, 3, 2]).flip([2]),
+                    13: lambda x: x.permute([0, 1, 3, 2]).flip([1,2]),
+                    14: lambda x: x.permute([0, 1, 3, 2]).flip([3]),
+                    15: lambda x: x.permute([0, 1, 3, 2]).flip([1,3]),
+                    16: lambda x: x.permute([0, 2, 3, 1]).flip([1,2]),
+                    17: lambda x: x.permute([0, 2, 3, 1]).flip([2,3]),
+                    18: lambda x: x.permute([0, 2, 3, 1]).flip([1,3]),
+                    19: lambda x: x.permute([0, 2, 3, 1]).flip([1,2,3]),
+                    20: lambda x: x.permute([0, 3, 1, 2]).flip([1,2]),
+                    21: lambda x: x.permute([0, 3, 1, 2]).flip([1,3]),
+                    22: lambda x: x.permute([0, 3, 1, 2]).flip([2,3]),
+                    23: lambda x: x.permute([0, 3, 1, 2]).flip([1,2,3]),
+                    24: lambda x: x}
+        '''
+        # Using custom pytorch re-indexing
+        self.rot = {1:  lambda x: flip_along(x,[1,2]),
+                    2:  lambda x: flip_along(x,[1,3]),
+                    3:  lambda x: flip_along(x,[2,3]),
+                    4:  lambda x: flip_along(x.permute([0, 2, 1, 3]),[1]),
+                    5:  lambda x: flip_along(x.permute([0, 2, 1, 3]),[1,3]),
+                    6:  lambda x: flip_along(x.permute([0, 2, 1, 3]),[2]),
+                    7:  lambda x: flip_along(x.permute([0, 2, 1, 3]),[2,3]),
+                    8:  lambda x: flip_along(x.permute([0, 3, 2, 1]),[1]),
+                    9:  lambda x: flip_along(x.permute([0, 3, 2, 1]),[1,2]),
+                    10: lambda x: flip_along(x.permute([0, 3, 2, 1]),[3]),
+                    11: lambda x: flip_along(x.permute([0, 3, 2, 1]),[2,3]),
+                    12: lambda x: flip_along(x.permute([0, 1, 3, 2]),[2]),
+                    13: lambda x: flip_along(x.permute([0, 1, 3, 2]),[1,2]),
+                    14: lambda x: flip_along(x.permute([0, 1, 3, 2]),[3]),
+                    15: lambda x: flip_along(x.permute([0, 1, 3, 2]),[1,3]),
+                    16: lambda x: flip_along(x.permute([0, 2, 3, 1]),[1,2]),
+                    17: lambda x: flip_along(x.permute([0, 2, 3, 1]),[2,3]),
+                    18: lambda x: flip_along(x.permute([0, 2, 3, 1]),[1,3]),
+                    19: lambda x: flip_along(x.permute([0, 2, 3, 1]),[1,2,3]),
+                    20: lambda x: flip_along(x.permute([0, 3, 1, 2]),[1,2]),
+                    21: lambda x: flip_along(x.permute([0, 3, 1, 2]),[1,3]),
+                    22: lambda x: flip_along(x.permute([0, 3, 1, 2]),[2,3]),
+                    23: lambda x: flip_along(x.permute([0, 3, 1, 2]),[1,2,3]),
+                    24: lambda x: x}
+        '''
+    def __call__(self, x, rand):
+        return self.rot[rand](x)
+
